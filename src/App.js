e1c91da299e2e536baba8b1c5b71c567a0562cf7
Chris Choi <chrischoi96@gmail.com>
2019-10-16 17:13:45 -0400

    basic authentification with Github

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+const CLIENT_ID = "9eef6e17d66411722d42";
+const REDIRECT_URI = "http://localhost:3000/";
+
 function App() {
+  const [status, setStatus] = useState("initial");
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    console.log(window.location.href);
+    const code =
+      window.location.href.match("?code=") &&
+      window.location.href.match("?code=")[1];
+    if (code) {
+      setStatus(() => "status_loading");
+      fetch(`https://gitstar.herokuapp.com/authenticate/${code}`)
+        .then(response => response.json())
+        .then(({ token }) => {
+          setStatus("finished_loading");
+          setToken(token);
+        });
+    }
+    console.log(code);
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <a
+        href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
+      >
+        Login
+      </a>
     </div>
   );
 }
