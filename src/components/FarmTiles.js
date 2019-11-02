c5a05b05171126a4a4a51b827c3b830724d6bf7c
Michael-Choi <Michael.choi@queensu.ca>
2019-11-02 15:13:02 -0400

    add details to crop hover

--- src/components/FarmTiles.js
+++ src/components/FarmTiles.js
@@ -2,17 +2,31 @@ import React, { useState } from "react";
 import styled from "styled-components";
 const FarmTiles = ({ className, img, habit }) => {
   const [showCropDetail, setShowCropDetail] = useState(false);
+
+  const showDeets = () => {
+    if (habit) {
+      setShowCropDetail(true);
+    }
+  };
+  const hideDeets = () => {
+    setShowCropDetail(false);
+  };
+
   return (
-    <div
-      className={`${className}`}
-      onMouseOver={() => {
-        if (habit) {
-          setShowCropDetail(true);
-        }
-      }}
-      onMouseLeave={() => setShowCropDetail(false)}
-    >
-      {showCropDetail && <StyledHover> {habit.crop_name}</StyledHover>}
+    <div className={className} onMouseOver={showDeets} onMouseLeave={hideDeets}>
+      {showCropDetail && (
+        <StyledHover>
+          <StyledUl>
+            <li>
+              {habit.crop_name[0].toUpperCase() + habit.crop_name.slice(1)}:
+              Lvl. {habit.crop_state}
+            </li>
+            <li>habit task: {habit.name}</li>
+            {habit.notes && <li>Notes: {habit.notes}</li>}
+            <li>Habit started: {habit.created_at.slice(0, 10)}</li>
+          </StyledUl>
+        </StyledHover>
+      )}
       <img className="soilTile" src={img} />
 
       {habit && (
@@ -25,10 +39,13 @@ const FarmTiles = ({ className, img, habit }) => {
   );
 };
 
+const StyledUl = styled.ul`
+  padding: 10px;
+  list-style: none;
+`;
 const StyledHover = styled.div`
   position: fixed;
-  background-color: white;
-  padding: 10px;
+  background-color: rgba(150, 255, 150, 0.6);
   z-index: 10;
 `;
 export default FarmTiles;
