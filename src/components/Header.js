3905d753b59cf9a37cdd9c48ab78f154406ab7a1
Chris Choi <chrischoi96@gmail.com>
2019-11-03 22:06:49 +0000

    revamped navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styled from "styled-components";
 
 const Header = ({ cookies, setMode, className, userCoin }) => {
-  console.log("userCoin inside navbar -> ", userCoin);
   if (userCoin) {
     let userCoinInfo = userCoin[0]["coin"];
     return (
-      <StyledNavbar className={className} expand="lg">
-        <StyledBrand onClick={() => setMode("farm")}>habyte</StyledBrand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" id="toggle" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav.Link onClick={() => setMode("farm")}>Farm</Nav.Link>
-          <Nav.Link onClick={() => setMode("new-habits")}>New Habits</Nav.Link>
-          <Nav.Link onClick={() => setMode("store")}>Store</Nav.Link>
-          <Nav.Link href="/new-habits">Past Habits</Nav.Link>
-          <Nav.Link>{userCoinInfo} Coin!</Nav.Link>
-          <NavDropdown
-            title={cookies.name || cookies.github_id}
-            id="basic-nav-dropdown"
-          >
-            <NavDropdown.Item href="/username">My Profile</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
-          </NavDropdown>
+      <StyledNavbar collapseOnSelect expand="md" className={className}>
+        <Navbar.Brand>habyte</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link onClick={() => setMode("farm")}>Farm</Nav.Link>
+            <NavDropdown
+              title="Categories"
+              id="basic-nav-dropdown"
+              className="navbar-right"
+            >
+              <NavDropdown.Item onClick={() => setMode("coding")}>
+                Coding
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={() => setMode("health")}>
+                Health
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link onClick={() => setMode("store")}>Store</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link>
+              <img
+                src="/assets/other/coin.png"
+                style={{ width: "25px" }}
+                alt="coin"
+              />{" "}
+              {userCoinInfo}
+            </Nav.Link>
+            <NavDropdown
+              title={cookies.name || cookies.github_id}
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="/username">My Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
         </Navbar.Collapse>
       </StyledNavbar>
     );
+  } else {
+    return <div></div>;
   }
-  return <div></div>;
 };
 
 const StyledNavbar = styled(Navbar)`
-  font-family: "Roboto", sans-serif;
-  font-weight: bold;
-  font-size: 1.1em;
-  color: #fff;
-  background: rgba(36, 204, 143);
-  #basic-nav-dropdown,
-  #basic-navbar-nav a {
-    color: #fff;
+  width: 95%;
+  margin: 0 auto;
+  .dropdown:hover .dropdown-menu {
+    display: block;
+    margin-right: 20%;
   }
-  .dropdown-menu.show {
-    background-color: rgba(36, 204, 143);
-    border: none;
+  .dropdown-toggle:after {
+    content: none;
   }
-  .dropdown-menu.show a:hover {
-    background-color: rgba(36, 204, 143);
-  }
-`;
-
-const StyledBrand = styled(Navbar)`
-  font-family: "Roboto", sans-serif;
-  font-size: 1.5em;
 `;
 
 export default Header;
