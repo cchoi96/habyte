fcc08a2448f3d6af588cc2417683289990a4208c
Michael-Choi <Michael.choi@queensu.ca>
2019-11-03 23:55:43 -0500

    fix resizing bug

--- src/containers/NewHabits.js
+++ src/containers/NewHabits.js
@@ -153,8 +153,10 @@ const CheckedStyledDiv = styled.div`
   .check {
     background-color: #c3c0c7;
     color: #a5a1ac;
-    height: 70px;
-    width: 50px;
+
+    height: 100%;
+    min-width: 50px;
+
     display: flex;
     align-items: center;
     justify-content: center;
@@ -172,6 +174,27 @@ const CheckedStyledDiv = styled.div`
     color: #a5a1ac;
     h5 {
       margin: 0;
+    }
+  }
+
+  @media only screen and (max-width: 950px) {
+    width: 100px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    .check {
+      width: 20px;
+      font-size: 0.8em;
+    }
+
+    .info {
+      width: 100px;
+      margin: 0 auto;
+      overflow-x: scroll;
+      align-items: center;
+      h5 {
+        font-size: 0.8em;
+      }
     }
   }
 `;
