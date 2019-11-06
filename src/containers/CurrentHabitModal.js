5ab15e328e1e5aa91ec61d0da82c0ccbef6fdae7
Michael-Choi <Michael.choi@queensu.ca>
2019-11-06 15:19:25 -0500

    remove unnecessary props for graph

--- src/containers/CurrentHabitModal.js
+++ src/containers/CurrentHabitModal.js
@@ -115,15 +115,7 @@ const CurrentHabitModal = ({ habit, setIsStatsOpen, isStatsOpen }) => {
           {modalData.length > 1 && (
             <XYPlot width={400} height={300}>
               <XAxis tickValues={[1, 2, 3, 4]} />
-              <YAxis
-                tickValues={[0, 1, 2, 3, 4, 5, 6, 7]}
-                includeMargin={false}
-                xPercent={0.06}
-                yPercent={0.06}
-              />
-
-              {/* <HorizontalGridLines />
-          <VerticalGridLines /> */}
+              <YAxis tickValues={[0, 1, 2, 3, 4, 5, 6, 7]} />
               <LineMarkSeries data={modalData} animation={"noWobble"} />
             </XYPlot>
           )}
